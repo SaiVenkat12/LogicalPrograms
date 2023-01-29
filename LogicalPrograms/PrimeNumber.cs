3883890d800b7e07752ec5e6ddd1f8d648b9aca7
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-29 22:54:43 +0530

    [Sai Venkat] Added Prime Number or not

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+        public static void Prime()
+        {
+            int count = 0;
+            Console.WriteLine("Enter the Number to check Prime Number or not : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= (n / 2); i++)
+            {
+                if (n % i == 0)
+                {
+                  count++;  
+                }
+            }
+            if(count<=2)
+            {
+                Console.WriteLine("{0} is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Prime Number", n);
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -14,7 +14,7 @@ namespace LogicalPrograms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.Fibonacci Series \n2.PerfectNumbert \n22.Exit");
+                Console.WriteLine("1.Fibonacci Series \n2.PerfectNumbert \n3.Prime Number \n22.Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -23,6 +23,9 @@ namespace LogicalPrograms
                         break;
                     case 2:
                         PerfectNumber.Perfect();
+                        break;
+                    case 3:
+                        PrimeNumber.Prime();
                         break;
                     case 22:
                         flag = false;
